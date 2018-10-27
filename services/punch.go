bb96b1d102d5edd9594fa3aa369f439953680489
Wesley Oliveira <wesleyholiveir@gmail.com>
2018-10-27 20:29:57 -0300

    Uncommented part of code for deal with home and isolated get requests on a specific method.

--- services/punch.go
+++ services/punch.go
@@ -1,20 +1,18 @@
 package services
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"net/http"
-
-	"golang.org/x/net/html"
+	"regexp"
 
 	log "github.com/sirupsen/logrus"
 	"github.com/wesleyholiveira/punchbot/helpers"
 	"github.com/wesleyholiveira/punchbot/models"
+	"golang.org/x/net/html"
 )
 
-func GetProjects(endpoint string) []models.Project {
-
-	projectMap := make(map[string]models.Project)
-	projects := make([]models.Project, 0)
-
+func Get(endpoint string) *http.Response {
 	resp, err := http.Get(endpoint)
 	if err != nil {
 		log.Error(err)
@@ -24,31 +22,42 @@ func GetProjects(endpoint string) []models.Project {
 		log.Errorf("%s %d", resp.Status, resp.StatusCode)
 	}
 
-	sResponse := resp.Body
-	defer resp.Body.Close()
-
 	if err != nil {
 		log.Error(err)
 	}
 
-	doc, err := html.Parse(sResponse)
-	if err != nil {
-		log.Error(err)
+	return resp
+}
+
+func GetProjects(endpoint string, typ models.GetProjectsType) []models.Project {
+
+	projectMap := make(map[string]models.Project)
+	projects := make([]models.Project, 0)
+
+	sResponse := Get(endpoint).Body
+	defer sResponse.Close()
+
+	if typ == models.Calendar {
+		doc, err := html.Parse(sResponse)
+		if err != nil {
+			log.Error(err)
+		}
+
+		helpers.Transverse(doc, &projects, projectMap, "")
+	} else {
+		r, _ := ioutil.ReadAll(sResponse)
+		re := regexp.MustCompile(`(\[.+\];)`)
+		response := re.Find(r)
+
+		re = regexp.MustCompile(`\\\/`)
+		response = re.ReplaceAll(response, []byte(`/`))
+		response = response[:len(response)-1]
+
+		err := json.Unmarshal(response, &projects)
+		if err != nil {
+			log.Error(err)
+		}
 	}
-
-	helpers.Transverse(doc, &projects, projectMap, "")
-
-	/* re := regexp.MustCompile(`(\[.+\];)`)
-	response := re.Find(sResponse)
-
-	re = regexp.MustCompile(`\\\/`)
-	response = re.ReplaceAll(response, []byte(`/`))
-	response = response[:len(response)-1]
-
-	err = json.Unmarshal(response, &projects)
-	if err != nil {
-		log.Error(err)
-	} */
 
 	return helpers.RemoveDuplicates(projects)
 }
