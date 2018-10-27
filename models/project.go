1ab733d470f0bf0e7bac3814a11d33c5227968f9
Wesley Oliveira <wesleyholiveir@gmail.com>
2018-10-27 20:32:18 -0300

    Added new fields and exposed the struct for projects from the calendars' page on a var

--- models/project.go
+++ models/project.go
@@ -3,14 +3,22 @@ package models
 type Project struct {
 	IDProject string `json:"id_projeto"`
 	Project   string `json:"projeto"`
+	Link      string `json:"link"`
+	Numero    string `json:"numero"`
+	Screen    string `json:"screen"`
 }
 
-var projects *[]Project
+var projects, calendarProjects *[]Project
 
 func init() {
 	projects = new([]Project)
+	calendarProjects = new([]Project)
 }
 
 func GetProjects() *[]Project {
 	return projects
+}
+
+func GetCalendarProjects() *[]Project {
+	return calendarProjects
 }
