92d14241de69eca2f968826050ecfe31f6b6a633
Wesley Oliveira <wesley.oliveira@zarpsystem.com.br>
2018-11-01 17:29:55 -0300

    Updated config file and vendor

--- configs/configs.go
+++ configs/configs.go
@@ -1,25 +1,25 @@
 package configs
 
-// import (
-// 	"os"
-// )
+import (
+	"os"
+)
 
-// // Settings
-// var Timer = os.Getenv("TIMER")
+// Settings
+var Timer = os.Getenv("TIMER")
 
-// // Discord
-// var AuthURL = os.Getenv("AUTH_URL")
+// Discord
+var AuthURL = os.Getenv("AUTH_URL")
 
-// var NotificationChannelsID = os.Getenv("NOTIFICATION_CHANNELS")
-// var CommandChannelsID = os.Getenv("COMMAND_CHANNELS")
-// var DiscordToken = os.Getenv("DISCORD_TOKEN")
+var NotificationChannelsID = os.Getenv("NOTIFICATION_CHANNELS")
+var CommandChannelsID = os.Getenv("COMMAND_CHANNELS")
+var DiscordToken = os.Getenv("DISCORD_TOKEN")
 
-// // Site
-// var PunchEndpoint = os.Getenv("PUNCHSITE")
-// var Home = PunchEndpoint + "/home"
-// var Calendar = PunchEndpoint + "/calendario"
+// Site
+var PunchEndpoint = os.Getenv("PUNCHSITE")
+var Home = PunchEndpoint + "/home"
+var Calendar = PunchEndpoint + "/calendario"
 
-// // Redis
-// var RedisHost = os.Getenv("REDIS_HOST")
-// var RedisPort = os.Getenv("REDIS_PORT")
-// var RedisPassword = os.Getenv("REDIS_PASSWORD")
+// Redis
+var RedisHost = os.Getenv("REDIS_HOST")
+var RedisPort = os.Getenv("REDIS_PORT")
+var RedisPassword = os.Getenv("REDIS_PASSWORD")
