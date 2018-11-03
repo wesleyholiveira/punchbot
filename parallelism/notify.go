3c7e2f01ea46b05f2548d0a69e1a76f69a91be0c
Wesley Oliveira <wesleyholiveir@gmail.com>
2018-11-03 20:23:47 -0300

    New module to get the list of notifications

---
+++ parallelism/notify.go
@@ -0,0 +1,36 @@
+package parallelism
+
+import (
+	"encoding/json"
+
+	"github.com/go-redis/redis"
+	log "github.com/sirupsen/logrus"
+	"github.com/wesleyholiveira/punchbot/models"
+)
+
+func Notify(redis *redis.Client) {
+	log.Info("Getting the personal notifications...")
+
+	notifyUser := models.GetNotifyUser()
+	notifyRedis := &models.Notify{}
+	redisKeys, err := redis.Keys("*").Result()
+
+	if err != nil {
+		log.Error(err)
+	}
+
+	for _, key := range redisKeys {
+		redisGet, err := redis.Get(key).Result()
+		if err != nil {
+			log.Errorln("Redis GET", err)
+		}
+
+		err = json.Unmarshal([]byte(redisGet), notifyRedis)
+
+		if err != nil {
+			log.Errorln("Unmarshal", err)
+		}
+
+		notifyUser[key] = notifyRedis
+	}
+}
