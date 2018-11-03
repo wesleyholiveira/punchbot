f9a1913b2c3abc683a74de636997f0a2b3c984dc
Wesley Oliveira <wesleyholiveir@gmail.com>
2018-11-03 20:21:00 -0300

    Added log in the project method

--- parallelism/project.go
+++ parallelism/project.go
@@ -1,12 +1,14 @@
 package parallelism
 
 import (
+	log "github.com/sirupsen/logrus"
 	"github.com/wesleyholiveira/punchbot/configs"
 	"github.com/wesleyholiveira/punchbot/models"
 	"github.com/wesleyholiveira/punchbot/services"
 )
 
 func GetProjects() {
+	log.Info("Getting the latest releases")
 	projects := models.GetProjects()
 	if len(*projects) == 0 {
 		*projects = services.GetProjects(configs.Home, models.Home)
