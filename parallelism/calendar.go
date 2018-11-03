6271888b2ba17179c0617c23187c6615cd040026
Wesley Oliveira <wesleyholiveir@gmail.com>
2018-11-03 20:19:24 -0300

    Added log in the calendar method

--- parallelism/calendar.go
+++ parallelism/calendar.go
@@ -1,12 +1,14 @@
 package parallelism
 
 import (
+	log "github.com/sirupsen/logrus"
 	"github.com/wesleyholiveira/punchbot/configs"
 	"github.com/wesleyholiveira/punchbot/models"
 	"github.com/wesleyholiveira/punchbot/services"
 )
 
 func GetProjectsCalendar() {
+	log.Info("Getting the projects from the calendars' page")
 	projectsCalendar := models.GetCalendarProjects()
 	if len(*projectsCalendar) == 0 {
 		*projectsCalendar = services.GetProjects(configs.Calendar, models.Calendar)
